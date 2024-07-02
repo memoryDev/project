5282ac35c6585ae653fe5ad634f487f034731e8b
memoryDev <115910144+memoryDev@users.noreply.github.com>
2024-07-02 11:19:55 +0900

    Initial commit

---
+++ non-member-board/backend/src/test/java/com/memory/board/NomMemberBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memory.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomMemberBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
