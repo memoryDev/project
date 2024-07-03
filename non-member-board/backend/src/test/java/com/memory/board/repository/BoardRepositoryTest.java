8ba13e6c6596a59f363973211161e3df4fb3912f
memoryDev <115910144+memoryDev@users.noreply.github.com>
2024-07-03 14:22:12 +0900

    ignore 추가 및 테스트코드 수정

--- non-member-board/backend/src/test/java/com/memory/board/repository/BoardRepositoryTest.java
+++ non-member-board/backend/src/test/java/com/memory/board/repository/BoardRepositoryTest.java
@@ -39,18 +39,6 @@ class BoardRepositoryTest {
     void selectBoardTest() {
         log.debug("{}", "===== 게시판 목록 조회 start =====");
 
-        //when
-        BoardDTO boardA = new BoardDTO("작성자A", "123456", "게시글 제목A", "게시글 내용A", LocalDateTime.now(), null, DeleteStatus.N);
-        BoardDTO boardB = new BoardDTO("작성자B", "123456", "게시글 제목B", "게시글 내용B", LocalDateTime.now(), null, DeleteStatus.N);
-        BoardDTO boardC = new BoardDTO("작성자C", "123456", "게시글 제목C", "게시글 내용C", LocalDateTime.now(), null, DeleteStatus.N);
-
-        em.persist(boardA.toEntity());
-        em.persist(boardB.toEntity());
-        em.persist(boardC.toEntity());
-
-        em.flush();
-        em.clear();
-
         //then
         List<Board> boardList = boardRepository.findAll();
 
@@ -59,7 +47,7 @@ class BoardRepositoryTest {
         Board findBoardB = boardRepository.findById(2L).orElse(new Board());
         Board findBoardC = boardRepository.findById(3L).orElse(new Board());
 
-        Assertions.assertThat(boardList.size()).isEqualTo(3);
+        Assertions.assertThat(boardList.size()).isEqualTo(15);
         Assertions.assertThat(boardList).contains(findBoardA, findBoardB, findBoardC);
 
         log.debug("{}", "===== 게시판 목록 조회 end =====");
--- non-member-board/backend/src/test/java/com/memory/board/service/BoardServiceTest.java
+++ non-member-board/backend/src/test/java/com/memory/board/service/BoardServiceTest.java
@@ -38,17 +38,6 @@ class BoardServiceTest {
     @Test
     @DisplayName("게시판 목록 조회")
     void selectBoardTest() {
-        //when
-        BoardDTO boardA = new BoardDTO("작성자A", "123456", "게시글 제목A", "게시글 내용A", LocalDateTime.now(), null, DeleteStatus.N);
-        BoardDTO boardB = new BoardDTO("작성자B", "123456", "게시글 제목B", "게시글 내용B", LocalDateTime.now(), null, DeleteStatus.N);
-        BoardDTO boardC = new BoardDTO("작성자C", "123456", "게시글 제목C", "게시글 내용C", LocalDateTime.now(), null, DeleteStatus.N);
-
-        em.persist(boardA.toEntity());
-        em.persist(boardB.toEntity());
-        em.persist(boardC.toEntity());
-
-        em.flush();
-        em.clear();
 
         //then
         List<Board> boardList = boardService.selectBoardList();
@@ -58,7 +47,7 @@ class BoardServiceTest {
         Board findBoardB = boardRepository.findById(2L).orElse(new Board());
         Board findBoardC = boardRepository.findById(3L).orElse(new Board());
 
-        Assertions.assertThat(boardList.size()).isEqualTo(3);
+        Assertions.assertThat(boardList.size()).isEqualTo(15);
         Assertions.assertThat(boardList).contains(findBoardA, findBoardB, findBoardC);
     }
 
