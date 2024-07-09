c45db313b45a5fde8061c010012b439f53ce950b
memoryDev <115910144+memoryDev@users.noreply.github.com>
2024-07-09 10:18:05 +0900

    기본경로 추가 및 페이지 오류시 나오는페이지 추가

--- non-member-board/frontend/src/App.jsx
+++ non-member-board/frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Board from "./pages/board/Board";
 import "bootstrap/dist/css/bootstrap.min.css";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -8,7 +9,9 @@ function App() {
       <div>
         <BrowserRouter>
           <Routes>
+            <Route path="/" element={<Board />}></Route>
             <Route path="/board/" element={<Board />}></Route>
+            <Route path="/*" element={<NotFound />}></Route>
           </Routes>
         </BrowserRouter>
       </div>
---
+++ non-member-board/frontend/src/pages/NotFound.jsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <div>잘못된 페이지 입니다. 추후수정 예정</div>;
+};
+
+export default NotFound;
